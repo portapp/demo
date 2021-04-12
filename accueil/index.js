d375fb9c69d66aeb88e9a545b885bf00370fc213
Clément Gauthier <clement.gauthier@hotmail.com>
2021-04-12 12:38:53 -0400

    carte

---
+++ accueil/index.js
@@ -0,0 +1,6 @@
+console.log(document.getElementsByTagName("a"))
+
+document.getElementsByTagName("page")[0].addEventListener("click", function() {
+    console.log("k")
+    window.open("../carte/index.html");
+});
