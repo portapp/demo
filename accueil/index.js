79ed913369dc1964a5090cfd62db15b33c2cacbd
Clément Gauthier <clement.gauthier@hotmail.com>
2021-04-26 19:03:44 -0400

    site

--- accueil/index.js
+++
@@ -1,6 +0,0 @@
-console.log(document.getElementsByTagName("a"))
-
-document.getElementsByTagName("page")[0].addEventListener("click", function() {
-    console.log("k")
-    window.open("../carte.pdf");
-});
